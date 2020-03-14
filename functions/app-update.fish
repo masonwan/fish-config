9194cd7f6154c9ad333524ed70f333edd1cd363c
Mason Wan <masonwan@gmail.com>
2020-03-14 10:19:14 -0700

    Update license year

--- functions/app-update.fish
+++ functions/app-update.fish
@@ -18,15 +18,15 @@ function app-update --description 'Update with Homebrew and Nodejs'
     brew cleanup
   end
 
-  if type -q yarn >/dev/null
-    log info 'Updating yarn modules...'
-    yarn global upgrade --latest --caret --slient --no-progress >/dev/null
-  end
-
   if type -q npm >/dev/null
     log info 'Updating NPM modules...'
     npm -g install npm
     npm -g update
+  end
+
+  if type -q yarn >/dev/null
+    log info 'Updating yarn modules...'
+    yarn global upgrade --latest --caret --slient --no-progress >/dev/null
   end
 
   if type -q fisher >/dev/null
