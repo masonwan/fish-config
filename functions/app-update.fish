6ba07098fa3fbe84fbfbae5885e8b3e4a38aff5c
Mason Wan <masonwan@users.noreply.github.com>
2022-02-24 23:17:52 -0800

    Make Node.js upgrade earlier than NPM module updates

--- functions/app-update.fish
+++ functions/app-update.fish
@@ -1,14 +1,16 @@
 function app-update --description 'Update with Homebrew and Nodejs'
-  set os (uname)
 
-  if test $os = 'Linux'
-    if type -q apt
-      log info 'Updating Advanced Package Tool (APT) packages...'
-      sudo apt update
-      sudo apt dist-upgrade -y
-      sudo apt upgrade -y
-      sudo apt autoremove -y
-    end
+  if type -q fisher >/dev/null
+    log info 'Updating fisherman modules...'
+    fisher update
+  end
+
+  if type -q apt
+    log info 'Updating Advanced Package Tool (APT) packages...'
+    sudo apt update
+    sudo apt dist-upgrade -y
+    sudo apt upgrade -y
+    sudo apt autoremove -y
   end
 
   if type -q brew
@@ -18,25 +20,15 @@ function app-update --description 'Update with Homebrew and Nodejs'
     brew cleanup
   end
 
+  if type -q nvm >/dev/null
+    log info 'Upgrading Node.js...'
+    nvm list-remote latest
+    nvm install latest
+  end
+
   if type -q npm >/dev/null
     log info 'Updating NPM modules...'
     npm -g install npm
     npm -g update
-  end
-
-  if type -q yarn >/dev/null
-    log info 'Updating yarn modules...'
-    yarn global upgrade --latest --caret --slient --no-progress >/dev/null
-  end
-
-  if type -q fisher >/dev/null
-    log info 'Updating fisherman modules...'
-    fisher update
-  end
-
-  if type -q nvm >/dev/null
-    log info 'Updating Node.js...'
-    nvm list-remote latest
-    nvm install latest
   end
 end
