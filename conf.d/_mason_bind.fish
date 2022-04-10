139a42ee384e7adad4cbde8e90d1e6f51a10979a
Mason Wan <masonwan@users.noreply.github.com>
2022-04-10 12:04:36 -0700

    Disable key binding when the shell is not interactive

--- conf.d/_mason_bind.fish
+++ conf.d/_mason_bind.fish
@@ -1,2 +1,4 @@
-bind -k npage nextd-or-forward-word
-bind -k ppage prevd-or-backward-word
+if status is-interactive
+  bind -k npage nextd-or-forward-word
+  bind -k ppage prevd-or-backward-word
+end
