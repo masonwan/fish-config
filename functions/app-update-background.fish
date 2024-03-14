4c963fa954ce0af2331c9ed070eaa8804dd5c620
Mason Wan <masonwan@users.noreply.github.com>
2024-03-14 11:11:53 -0700

    Use local variable

--- functions/app-update-background.fish
+++ functions/app-update-background.fish
@@ -1,5 +1,5 @@
 function app-update-background -d 'Update apps in the background'
-  set session_name server
+  set -l session_name server
   tmux has-session -t $session_name 2>/dev/null
   if [ $status -eq 1 ]
     tmux new -d -s $session_name
