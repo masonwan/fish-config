0676600f15e8b8ecddc6f00773c98bd5cbc07447
Mason Wan <masonwan@users.noreply.github.com>
2024-05-11 15:25:37 -0700

    Add nala to update packages

--- functions/app-update.fish
+++ functions/app-update.fish
@@ -9,7 +9,11 @@ function app-update -d 'Update apps'
 
   # `/usr/bin/apt` is "annotation processing tool" on MacOS. So here it checks the platform first.
   if test (uname) = 'Linux'
-    if type -q apt
+    if type -q nala
+      log info 'Updating Advanced Package Tool (APT) packages with nala...'
+      sudo nala upgrade -y
+      sudo apt autoremove -y
+    else if type -q apt
       log info 'Updating Advanced Package Tool (APT) packages...'
       sudo apt update
       sudo apt dist-upgrade -y
