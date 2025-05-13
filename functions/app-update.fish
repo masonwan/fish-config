f58e03af89890a3e92bb46752e7bfd49f108fb7c
Mason Wan <masonwan@gmail.com>
2025-05-12 23:47:38 -0700

    Refactor indentation in app-update function for improved readability

--- functions/app-update.fish
+++ functions/app-update.fish
@@ -1,22 +1,22 @@
 function app-update -d 'Update apps'
-  if type -q fd
-    set command "fd"
-    log info "Using fd as the command."
-  else if type -q fdfind
-    set command "fdfind"
-    log info "Using fdfind as the command."
-  else
-    log error "Cannot find fd or fdfind."
-    exit 1
-  end
+    if type -q fd
+        set command fd
+        log info "Using fd as the command."
+    else if type -q fdfind
+        set command fdfind
+        log info "Using fdfind as the command."
+    else
+        log error "Cannot find fd or fdfind."
+        exit 1
+    end
 
-  set -l files ($command ".\.fish" ~/repos/server-setup/updates/)
+    set -l files ($command ".\.fish" ~/repos/server-setup/updates/)
 
-  for file in $files
-    log info "Runnning $file..."
-    fish $file &
-  end
+    for file in $files
+        log info "Runnning $file..."
+        fish $file &
+    end
 
-  log info "Waiting for all update scripts to finish..."
-  wait fish
+    log info "Waiting for all update scripts to finish..."
+    wait fish
 end
