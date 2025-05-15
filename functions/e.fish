d26f1333756cd236818c7089ff949df113b6ddf5
Mason Wan <masonwan@users.noreply.github.com>
2025-05-15 16:58:04 -0700

    Update e.fish

--- functions/e.fish
+++ functions/e.fish
@@ -2,9 +2,7 @@ function e -d 'Start the vim with proper permission'
     set -l editors
     set -a editors /usr/bin/editor # Use update-alternatives, Debian's CLI version management.
     set -a editors hx # Helix
-    set -a editors nvim
-    set -a editors vim
-    set -a editors vi
+    set -a editors nvim vim vi
     set -a editors nano
 
     for editor in $editors
@@ -15,12 +13,12 @@ function e -d 'Start the vim with proper permission'
 
     set -l owner ''
     set -l os (uname)
-    if test $os = Darwin
+    if [ $os = Darwin ]
         set owner (stat -f '%Su' $argv)
     else
         set owner (stat -c '%U' $argv)
     end
-    if test $owner = root
+    if [ $owner = root ]
         log warn 'This file is owned by root, opening with sudo'
         sleep 1
         sudo -e $argv
