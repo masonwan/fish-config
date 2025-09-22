b03276eb67b5b84f7b332452894329a5e85d4dfb
Mason Wan <masonwan@users.noreply.github.com>
2025-09-22 15:16:31 -0700

    Update d.fish

--- functions/d.fish
+++ functions/d.fish
@@ -1,7 +1,10 @@
-function d -d 'Delete files. Use `trash-put` if available. Otherwise it will prompt to use `rm`.'
+function d --description 'Delete files. Use `trash-put` if available. Otherwise it will prompt to use `rm`.'
   if type -q trash-put
     log warn "Trashing file '$argv'"
     trash-put $argv
+  else if type -q trash
+    log warn "Trashing file '$argv'"
+    trash $argv
   else
     log error 'No trash program is available. The deleted file will not recoverable.'
     rm -ri $argv
