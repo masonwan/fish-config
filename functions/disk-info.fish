3b618f62fa8a563c843e39f01d351b7abefc1227
Mason Wan <masonwan@users.noreply.github.com>
2025-10-24 23:23:05 -0700

    Update disk-info.fish

--- functions/disk-info.fish
+++ functions/disk-info.fish
@@ -1,3 +1,3 @@
-function disk-info -d 'Show the disk name, size, and model'
-  lsblk -o NAME,SIZE,FSTYPE,MODEL,MOUNTPOINT,UUID,LABEL
+function disk-info -d 'Show the drive, partition, and mount info'
+  lsblk -o MODEL,SERIAL,NAME,SIZE,FSTYPE,MOUNTPOINT,LABEL
 end
