e3f4ada8df00eaa25ea3fc10992e20741fb46d4b
Mason Wan <masonwan@gmail.com>
2025-10-24 23:54:45 -0700

    Clean up

--- functions/e.fish
+++ functions/e.fish
@@ -1,28 +1,30 @@
 function e -d 'Start the vim with proper permission'
-    set -l editors
-    set -a editors /usr/bin/editor # Use update-alternatives, Debian's CLI version management.
-    set -a editors hx # Helix
-    set -a editors nvim vim vi
-    set -a editors nano
+  set -l editors
+  set -a editors /usr/bin/editor # Use update-alternatives, Debian's CLI version management.
+  set -a editors hx # Helix
+  set -a editors nvim vim vi
+  set -a editors nano
 
-    for editor in $editors
-        if command -q $editor
-            break
-        end
+  for editor in $editors
+    if command -q $editor
+      break
     end
+  end
 
-    set -l owner ''
-    set -l os (uname)
-    if [ $os = Darwin ]
-        set owner (stat -f '%Su' $argv)
-    else
-        set owner (stat -c '%U' $argv)
-    end
-    if [ $owner = root ]
-        log warn 'This file is owned by root, opening with sudo'
-        sleep 1
-        sudo -e $argv
-    else
-        $editor $argv
-    end
+  set -l owner ''
+  set -l os (uname)
+  set -l file $argv[1]
+
+  if [ $os = Darwin ]
+    set owner (stat -f '%Su' $file)
+  else
+    set owner (stat -c '%U' $file)
+  end
+  if [ $owner = root ]
+    log warn 'This file is owned by root, opening with sudo'
+    sleep 1
+    sudo -e $file
+  else
+    $editor $file
+  end
 end
