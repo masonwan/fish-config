9cc03a7946591f88e0cedbe57f2718560596d948
Mason Wan <masonwan@users.noreply.github.com>
2026-03-01 09:49:18 -0800

    Update trash-related abbreviations in _mason-alias.fish

--- conf.d/_mason-alias.fish
+++ conf.d/_mason-alias.fish
@@ -5,8 +5,8 @@ alias pf 'ps -ef | rg'
 alias ipinfo 'curl -s ipinfo.io | jq'
 alias ipinfo2 'curl api.ip2location.io | jq'
 alias ssh-speed-test "dd if=/dev/zero bs=1G count=1 | pv -s 1G | ssh mwan.dev 'cat > /dev/null'"
-abbr find-trashes "fdfind --hidden -E .Trash-1000 '^\._'"
-abbr trash-trashes "d (fdfind --hidden -E .Trash-1000 '^\._')"
+abbr find-trashes "fdfind --hidden -E .Trash-1000 -E Trash '^\._'"
+abbr trash-trashes "d (fdfind --hidden -E .Trash-1000 -E Trash '^\._')"
 
 function find-string -d 'Find string in all files under current directory. http://explainshell.com/explain?cmd=grep+-rnw+.+-H+-e+%24argv '
   grep -rni * -e $argv
