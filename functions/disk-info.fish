85dd0b23d8795301dbe1527fdccddf0411ecc18c
Mason Wan <masonwan@users.noreply.github.com>
2026-05-03 21:39:16 -0700

    Add FSUSE% to disk-info output

--- functions/disk-info.fish
+++ functions/disk-info.fish
@@ -1,3 +1,3 @@
 function disk-info -d 'Show the drive, partition, and mount info'
-  lsblk -o MODEL,SERIAL,NAME,SIZE,FSTYPE,MOUNTPOINT,LABEL
+  lsblk -o MODEL,SERIAL,NAME,SIZE,FSUSE%,FSTYPE,MOUNTPOINT,LABEL
 end
