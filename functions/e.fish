bd5d49fad1771b9e4c1002746842408c2807ba93
Mason Wan <masonwan@gmail.com>
2026-07-02 14:11:50 -0700

    refactor(e): rewrite e function to check EDITOR, handle non-existent files, and use sudo -e

--- functions/e.fish
+++ functions/e.fish
@@ -1,31 +1,30 @@
-function e -d 'Start the vim with proper permission'
-  set -l editors
-  set -a editors /usr/bin/editor # Use update-alternatives, Debian's CLI version management.
-  set -a editors hx # Helix
-  set -a editors helix # In CachyOS, `helix` instead of `hx`
-  set -a editors nvim vim vi
-  set -a editors nano
-
-  for editor in $editors
-    if command -q $editor
-      break
-    end
+function e -d 'Edit a file with proper permission'
+  if not set -q EDITOR
+    log warn '$EDITOR is not set'
+    return 1
   end
 
-  set -l owner ''
-  set -l os (uname)
   set -l file $argv[1]
 
-  if [ $os = Darwin ]
-    set owner (stat -f '%Su' $file)
-  else
-    set owner (stat -c '%U' $file)
+  if not test -e "$file"
+    log warn "File does not exist, creating: $file"
+    sleep 1
+    touch "$file"
   end
-  if [ $owner = root ]
+
+  # Check if the file is owned by root.
+  set -l owner
+  if test (uname) = Darwin
+    set owner (stat -f '%Su' "$file")
+  else
+    set owner (stat -c '%U' "$file")
+  end
+
+  if test "$owner" = root
     log warn 'This file is owned by root, opening with sudo'
     sleep 1
-    sudo -e $file
+    sudo -e "$file"
   else
-    $editor $file
+    $EDITOR "$file"
   end
 end
