d1608cf15c5c48cbfc60d9f0c0ae388743281e30
Mason Wan <masonwan@gmail.com>
2026-07-02 16:18:26 -0700

    refactor(e): remove auto-create file behavior

--- functions/e.fish
+++ functions/e.fish
@@ -6,12 +6,6 @@ function e -d 'Edit a file with proper permission'
 
   set -l file $argv[1]
 
-  if not test -e "$file"
-    log warn "File does not exist, creating: $file"
-    sleep 1
-    touch "$file"
-  end
-
   # Check if the file is owned by root.
   set -l owner
   if test (uname) = Darwin
