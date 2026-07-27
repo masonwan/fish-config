93028a1377aaf83f751740aed140bebdc3ba00f0
Mason Wan <masonwan@gmail.com>
2026-07-27 08:47:53 -0700

    Update d.fish function

--- functions/d.fish
+++ functions/d.fish
@@ -1,12 +1,18 @@
-function d --description 'Delete files. Use `trash-put` if available. Otherwise it will prompt to use `rm`.'
-  if type -q trash-put # For Linux
-    log warn "Trashing file '$argv'"
-    trash-put $argv
-  else if type -q trash # For MacOS
-    log warn "Trashing file '$argv'"
-    trash $argv
-  else
-    log error 'No trash program is available. The deleted file will not recoverable.'
-    rm -ri $argv
-  end
+function d --description 'Delete files using trashy, trash-cli, or fallback to rm'
+    if test (count $argv) -eq 0
+        log warn "No files specified for deletion."
+        return 1
+    end
+
+    if type -q trash
+        log warn "Trashing via trashy: $argv"
+        trash put $argv
+    else if type -q trash-put
+        log warn "Trashing via trash-cli: $argv"
+        trash-put $argv
+    else
+        log error "No trash program available. Files will be permanently deleted with confirmation."
+        rm -ri $argv
+    end
 end
+
